509d3d3bee3653279511ffc6dc65f255bf6db045
dvher <diego.vergara5@mail.udp.cl>
2022-08-19 23:52:57 -0400

    Added CSRF and Session

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import db, { escape } from '../database/db';
+import db from '../database/db';
 import argon2 from 'argon2';
 
 const register = async (req: Request, res: Response) => {
-    let { email, user, pass }: { email: string, user: string, pass: string } = req.body;
+    let { email, user, pass, confirmpass }: { email: string, user: string, pass: string, confirmpass: string } = req.body;
     if(!email || !user || !pass){
         res.status(400).json({status: 'ERROR', error: 'Parámetros inválidos'});
         return;
@@ -12,13 +12,26 @@ const register = async (req: Request, res: Response) => {
         res.status(400).json({status: 'ERROR', error: 'Parámetros inválidos'});
         return;
     }
+    if(pass != confirmpass){
+        res.status(400).json({status: 'ERROR', error: 'Contraseñas no coinciden'});
+        return;
+    }
+    if(pass.length < 10){
+        res.status(400).json({status: 'ERROR', error: 'Contraseña muy corta'});
+        return;
+    }
+    if(user.length > 20){
+        res.status(400).json({status: 'ERROR', error: 'Nombre de usuario muy largo'});
+        return;
+    }
     let password = await argon2.hash(`${user}.${pass}`, {
         type: argon2.argon2i,
-        timeCost: 5,
-        hashLength: 45
+        timeCost: 25,
+        hashLength: 45,
+        memoryCost: 2048
     });
     let sent: boolean = false;
-    db.query('SELECT user FROM pp_users WHERE user=? OR email=?;', [escape(user), escape(email)], (err, results: any) => {
+    db.query('SELECT user FROM pp_users WHERE user=? OR email=?;', [user, email], (err, results: any) => {
         if(err){
             !sent && res.status(400).json({status: 'ERROR', error: 'Ocurrió un error inesperado.'});
             sent = true;
@@ -27,7 +40,7 @@ const register = async (req: Request, res: Response) => {
                 !sent && res.status(400).json({status: 'ERROR', error: 'El usuario ya existe.'});
                 sent = true;
             }else{
-                db.query('INSERT INTO pp_users (email, user, password) VALUES (?, ?, ?);', [escape(email), escape(user), password], (err) => {
+                db.query('INSERT INTO pp_users (email, user, password) VALUES (?, ?, ?);', [email, user, password], (err) => {
                     if(err){
                         res.status(400).json({status: 'ERROR', error: 'Ocurrió un error inesperado.'});
                         sent = true;
@@ -51,13 +64,22 @@ const login = async (req: Request, res: Response) => {
         res.status(400).json({status: 'ERROR', error: 'Parámetros inválidos'});
         return;
     }
+    if(user.length > 20){
+        res.status(400).json({status: 'ERROR', error: 'Nombre de usuario muy largo'});
+        return;
+    }
+    if(pass.length < 10){
+        res.status(400).json({status: 'ERROR', error: 'Contraseña muy corta'});
+        return;
+    }
     let sent = false;
 
     const set = async (password: string, admin_status: boolean) => {
         let isValid = await argon2.verify(password, `${user}.${pass}`, {
             type: argon2.argon2i,
-            timeCost: 5,
-            hashLength: 45
+            timeCost: 25,
+            hashLength: 45,
+            memoryCost: 2048
         });
         if(isValid)
             !sent && res.status(200).json({status: admin_status ? 'OKADM' : 'OK', user, error: ''});
@@ -65,7 +87,7 @@ const login = async (req: Request, res: Response) => {
             !sent && res.status(400).json({status: 'ERROR', user, error: 'Credenciales inválidas'});
     }
     
-    db.query('SELECT password, isadmin FROM pp_users WHERE user=?;', [escape(user)], (err, results: any) => {
+    db.query('SELECT password, isadmin FROM pp_users WHERE user=?;', [user], (err, results: any) => {
         if(err){
             res.status(400).json({status: 'ERROR', user, error: 'Ocurrió un error inesperado.'});
             sent = true;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import bodyParser from 'body-parser';   
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import csrf from 'csurf';
+
 dotenv.config();
-import db from './database/db';
 const app = express();
 const port = 5000;
-import routes from './routes'
+import routes from './routes';
+
+const csrfProtection = csrf({ cookie: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(csrfProtection);
+app.use(cookieParser());
 app.use(cors());
 app.use(routes);
+
+app.get('/getCSRFToken', (req, res) => {
+
+    res.json({ CSRFToken: req.csrfToken() })
+
+});
 
 app.get('/', (req, res) => {
     res.send(`${process.env.DB_DATABASE}`);
